eed09c73a74f9e237aa0520e1b387e260d9d7a78
HeffieBoi <78382851+HeffieBoi@users.noreply.github.com>
2024-08-23 12:38:45 -0400

    Add files via upload
    Bridge Update

--- ros2_ws/src/px4_ros_com_modified/bridge.cpp
+++ ros2_ws/src/px4_ros_com_modified/bridge.cpp
@@ -4,7 +4,8 @@
 #include <px4_msgs/msg/vehicle_control_mode.hpp>
 #include <px4_msgs/msg/sensor_combined.hpp>
 
-#include <geometry_msgs/msg/PoseStamped.hpp>
+#include <px4_msgs/msg/vehicle_local_position.hpp>
+#include <geometry_msgs/msg/pose_stamped.hpp>
 
 #include <rclcpp/rclcpp.hpp>
 #include <stdint.h>
@@ -26,6 +27,7 @@ class GPSBridge : public rclcpp::Node {
 
 			subscription_ = this->create_subscription<px4_msgs::msg::SensorCombined>("/fmu/out/sensor_combined", qos,
 			[this](const px4_msgs::msg::SensorCombined::UniquePtr msg) {
+				/*
 				std::cout << "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n";
 				std::cout << "RECEIVED SENSOR COMBINED DATA"   << std::endl;
 				std::cout << "============================="   << std::endl;
@@ -39,25 +41,35 @@ class GPSBridge : public rclcpp::Node {
 				std::cout << "accelerometer_m_s2[1]: " << msg->accelerometer_m_s2[1] << std::endl;
 				std::cout << "accelerometer_m_s2[2]: " << msg->accelerometer_m_s2[2] << std::endl;
 				std::cout << "accelerometer_integral_dt: " << msg->accelerometer_integral_dt << std::endl;
+				*/
+				
+				ms2_roll = msg->gyro_rad[0];
+				ms2_pitch = msg->gyro_rad[1];
+				ms2_yaw = msg->gyro_rad[2];
 
-				Sensor_data = msg;
 			});
 
-			Opti_sub = this->create_subscription<geometry_msgs::msg::PoseStamped>(" name of opti topic", qos,
+			Opti_sub = this->create_subscription<geometry_msgs::msg::PoseStamped>("/vrpn_mocap/TestBody001/pose", qos,
 			[this](const geometry_msgs::msg::PoseStamped::UniquePtr msg) {
-				Opti_data = msg;
+
+				Opti_x = msg->pose.position.x;
+				Opti_y = msg->pose.position.y;
+				Opti_z = msg->pose.position.z;
+
 			});
 
 			vehicle_local_position_pub = this->create_publisher<VehicleLocalPosition>("/fmu/out/vehicle_local_position", 10);
 
 			auto timer_callback = [this]() -> void {
-				this->publish_local_position(Sensor_data, Opti_data);
+				this->publish_local_position(Opti_x, Opti_y, Opti_z);
 			};
 
 			timer_ = this->create_wall_timer(100ms, timer_callback);
 		}
 
     private:
+		rclcpp::TimerBase::SharedPtr timer_;
+
         rclcpp::Subscription<px4_msgs::msg::SensorCombined>::SharedPtr subscription_;
         rclcpp::Subscription<geometry_msgs::msg::PoseStamped>::SharedPtr Opti_sub;
 
@@ -66,15 +78,28 @@ class GPSBridge : public rclcpp::Node {
 		px4_msgs::msg::SensorCombined::UniquePtr Sensor_data;
 		geometry_msgs::msg::PoseStamped::UniquePtr Opti_data;
 
-        void publish_local_position(px4_msgs::msg::SensorCombined::UniquePtr Sensor_data,
-									geometry_msgs::msg::PoseStamped::UniquePtr Opti_data);
+        void publish_local_position(float Opti_x, float Opti_y, float Opti_z);
 
+		float Opti_x;
+		float Opti_y;
+		float Opti_z;
 
-}
+		float ms2_roll;
+		float ms2_pitch;
+		float ms2_yaw;
 
-void GPSBridge::publish_local_position(Sensor_data, Opti_data)
+};
+
+void GPSBridge::publish_local_position(float Opti_x, float Opti_y, float Opti_z)
 {
 	VehicleLocalPosition msg{};
+
+	msg.xy_valid = true;
+	msg.z_valid = true;
+
+	msg.x = Opti_x;
+	msg.y = -Opti_y;
+	msg.z = -Opti_z;
 
 	vehicle_local_position_pub->publish(msg);
 }
